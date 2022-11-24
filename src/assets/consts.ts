fe4759eeaef8e249b19d3dedcfa6c4c61ac9bfcb
Anna Mirocha <annamirocha93@gmail.com>
2022-11-24 23:17:52 +0100

    add basic files and folders structure

---
+++ src/assets/consts.ts
@@ -0,0 +1,14 @@
+export const tags: string[] = [
+    'All',
+    'UI',
+    'UX',
+    'Enhancement',
+    'Bug',
+    'Feature',
+]
+
+export const roadmapOptions = {
+    Planned: 'orange',
+    'In-Progress': 'purple',
+    Live: 'blue',
+}
