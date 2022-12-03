dadcce1692d259f7ebe713fdf8edda6a55b42016
Anna Mirocha <annamirocha93@gmail.com>
2022-12-03 14:39:07 +0100

    remove firebase config

--- src/api/firebase.config.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBatQucjhgU1C9YdGwdRIkaTxG_DwngjCA",
-    authDomain: "product-feedback-app-c28e5.firebaseapp.com",
-    projectId: "product-feedback-app-c28e5",
-    storageBucket: "product-feedback-app-c28e5.appspot.com",
-    messagingSenderId: "288523542354",
-    appId: "1:288523542354:web:c1648c1e37a03e240e648b"
-};
