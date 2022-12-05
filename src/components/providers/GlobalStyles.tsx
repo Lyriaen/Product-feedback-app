f861173913f0b8c5d57f5107191b5ab490ba998b
Anna Mirocha <annamirocha93@gmail.com>
2022-12-05 20:04:03 +0100

    add box-sizing and margin 0 to all elements

--- src/components/providers/GlobalStyles.tsx
+++ src/components/providers/GlobalStyles.tsx
@@ -1,14 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  html {
-    font-size: 10px;
-  }
-  
-  body {
-    margin: 0;
-    padding: 0;
-    background-color: #F7F8FD;
-    font-family: 'Jost', sans-serif;
-  }
-`;
+    html {
+        font-size: 10px;
+    }
+    * {
+        box-sizing: border-box;
+        margin: 0;
+    }
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #F7F8FD;
+        font-family: 'Jost', sans-serif;
+    }
+    `
