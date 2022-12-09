3474a8574ea4497ee919ea8c2aedff5e5eb1777a
Anna Mirocha <annamirocha93@gmail.com>
2022-12-09 22:21:15 +0100

    delate console log

--- src/components/pages/projectListPage/ProjectsListPage.tsx
+++ src/components/pages/projectListPage/ProjectsListPage.tsx
@@ -1,19 +1,15 @@
 import { useEffect , useState } from 'react';
 import { getProjectsList } from '../../../getData/getData';
 import { ProjectsList } from './project.class';
-import { MediumHeading , NormalText } from "../../common/common.styled";
-import { StyledUL, StyledLI } from "./ProjectsList.styled";
-import { Link } from "react-router-dom";
+import { MediumHeading , NormalText } from '../../common/common.styled';
+import { StyledUL, StyledLI } from './ProjectsList.styled';
+import { Link } from 'react-router-dom';
 
 export const ProjectsListPage = () => {
-
     const [projectsList, setProjectsList]= useState<ProjectsList | null>(null)
-
     useEffect(() => {
         getProjectsList(setProjectsList)
     }, [])
-    console.log(projectsList)
-
     return (
         <StyledUL>
             {projectsList && projectsList.getProjectsList.map(project => {
